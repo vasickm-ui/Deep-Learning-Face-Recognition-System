81cbb12abc3e6b79ff7dad74c191e346a88997a9
Kristina <dnikolic@industrial-it.software>
2026-06-17 14:12:00 +0200

    Adding functions for calculating parameters and printing them to console

--- face_detection/src/baseline_testing.py
+++ face_detection/src/baseline_testing.py
@@ -30,7 +30,8 @@ def calculate_avg_embeddings(enroll_path):
             img = cv2.imread(img_path)
 
             if img is None:
-                continue
+                print(f"Cant load image with path {img_path}")
+                exit()
 
             emb = get_embedding(img)
 
@@ -46,6 +47,101 @@ def calculate_avg_embeddings(enroll_path):
         emb_data[person] = avg_emb
     
     return emb_data
+
+#calculating system performance metrics for test data set
+def test_data_metrics(data_dict, test_path, threshold):
+
+    misidentification, false_reject = 0, 0
+    correct, n = 0, 0
+    #going through folders (djokovic, nadal, federer)
+    for person in os.listdir(test_path):
+
+        person_folder = os.path.join(test_path, person)
+        #going throgh image files
+        for img_name in os.listdir(person_folder):
+            img_path = os.path.join(person_folder,img_name)
+            img = cv2.imread(img_path)
+
+            if img is None:
+                print(f"Cant load image with path {img_path}")
+                exit()
+
+            emb = get_embedding(img)
+
+            best_similarity = -1
+            best_name = "Unknown"
+
+            #going through all avg embeddings
+            for name, avg_emb in data_dict.items():
+                cos_sim = cosine_similarity(emb, avg_emb)
+
+                if cos_sim > threshold and cos_sim > best_similarity:
+                    best_name = name
+                    best_similarity = cos_sim
+
+            if best_name == person:
+                correct += 1
+            elif best_name == "Unknown":
+                false_reject += 1
+            else:
+                misidentification +=1
+            
+            n += 1
+
+    print(f"Correct: {correct}/{n}")
+    print(f"Misidentification: {misidentification}/{n}")
+    print(f"False reject: {false_reject}/{n}")
+
+def unknown_data_metrics(data_dict, unknown_path, threshold):
+
+    false_accept = 0
+    correct, n = 0, 0
+
+    
+    #going throgh image files
+    for img_name in os.listdir(unknown_path):
+        img_path = os.path.join(unknown_path,img_name)
+        img = cv2.imread(img_path)
+
+        if img is None:
+            print(f"Cant load image with path {img_path}")
+            exit()
+
+        emb = get_embedding(img)
+
+        best_similarity = -1
+        best_name = "Unknown"
+
+        #going through all avg embeddings
+        for name, avg_emb in data_dict.items():
+            cos_sim = cosine_similarity(emb, avg_emb)
+
+            if cos_sim > threshold and cos_sim > best_similarity:
+                best_name = name
+                best_similarity = cos_sim
+
+        if best_name == "Unknown":
+            correct += 1
+        else:
+            false_accept +=1
+        
+        n += 1
+
+    print(f"Correct: {correct}/{n}")
+    print(f"False accept: {false_accept}/{n}")
+
+data_base = calculate_avg_embeddings("data/enroll")
+unknown_data_metrics(data_base, "data/unknown", 0.65)
+
+            
+
+
+            
+
+
+
+
+
 
 
 
